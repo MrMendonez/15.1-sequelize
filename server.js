9ec35e8c4392239f48f8f32cfebd29afe77c2eef
MrMendonez <dmendonez@gmail.com>
2016-02-15 19:28:20 -0500

    made login, register, and secret page

--- server.js
+++ server.js
@@ -13,6 +13,42 @@ var exphbs = require('express-handlebars');
 var PORT = process.env.NODE_ENV || 8000;
 var app = express();
 
+var connection = mysql.createConnection({
+  port: 3306,
+  host: 'localhost',
+  user: 'root',
+  database: 'rcb_authentication_db'
+});
+
 //Serve static content for the app from the "public" directory in the application directory.
 app.use("/static", express.static("public"));
 
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+  res.render('register');
+});
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.get('/secret', function(req, res) {
+  res.render('secret');
+});
+
+app.get('/*', function(req, res) {
+  res.render('/');
+});
+
+app.listen(PORT, function(){
+  console.log('Listening on %s', PORT)
+});
